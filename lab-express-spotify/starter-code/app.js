388c9abd890ac196ab44c9c7f45cca36f868694d
mjarraya <mjarraya@student.42.fr>
2020-02-03 09:33:02 +0100

    iteration 5

--- lab-express-spotify/starter-code/app.js
+++ lab-express-spotify/starter-code/app.js
@@ -58,6 +58,17 @@ app.get("/albums/:artistId", (req, res) => {
     });
 });
 
+app.get("/tracks/:albumId", (req, res) => {
+  spotifyApi
+    .getAlbumTracks(req.params.albumId)
+    .then(data => {
+      res.render("tracks.hbs", { tracks: data.body.items });
+    })
+    .catch(err => {
+      console.log("The error while searching tracks occurred: ", err);
+    });
+});
+
 app.listen(3000, () =>
   console.log("My Spotify project running on port 3000 🎧 🥁 🎸 🔊")
 );
